4e59365eefa1bb1e9e1bf60bbbac83949b50be1d
maratdev0210 <maratforwork1@gmail.com>
2025-01-30 13:03:03 +0300

    CityData css refactor

--- src/DataDisplay/CityData.jsx
+++ src/DataDisplay/CityData.jsx
@@ -1,14 +1,25 @@
 export default function CityData({ dataValues }) {
+  const cityData = [
+    {
+      name: "City, State",
+      value: dataValues[6],
+    },
+    {
+      name: "Zip Code",
+      value: dataValues[7],
+    },
+  ];
+
+  const components = cityData.map((data) => (
+    <div className="w-1/2 flex flex-col">
+      <span className="text-2xl font-bold text-blue-900">{data.name}</span>
+      <span className="text-xl">{data.value}</span>
+    </div>
+  ));
+
   return (
     <div className="city-data flex px-3 py-3 border-b-2 border-blue-900/50">
-      <div className="city w-1/2 flex flex-col">
-        <span className="text-2xl font-bold text-blue-900">City, State</span>
-        <span className="text-xl">{dataValues[6]}</span>
-      </div>
-      <div className="postal w-1/2 flex flex-col">
-        <span className="text-2xl font-bold text-blue-900">Zip Code</span>
-        <span className="text-xl">{dataValues[7]}</span>
-      </div>
+      {components}
     </div>
   );
 }
