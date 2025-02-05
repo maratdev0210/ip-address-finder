468be697ce80a482d4a0ec4f0275b0f97168e164
maratdev0210 <maratforwork1@gmail.com>
2025-02-05 19:22:23 +0300

    mobile menu added

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { CiMenuBurger } from "react-icons/ci";
+import { useState } from "react";
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
   const links = {
     "#": "Home",
     "#findIp": "Find your IP",
     "#aboutIp": "What's IP?",
   };
+
+  // doesn't allow style-imposed bugs when the screen changes
+  window.addEventListener("resize", (event) =>
+    event.target.innerWidth >= 560 ? setIsOpen(false) : null
+  );
 
   const navigationElements = Object.keys(links).map((link) => {
     return (
@@ -13,6 +21,7 @@ export default function Header() {
         <a
           className="text-white text-2xl xl:text-3xl hover:text-white/50 hover:underline"
           href={link}
+          onClick={() => setIsOpen(!isOpen)}
         >
           {links[link]}
         </a>
@@ -21,12 +30,24 @@ export default function Header() {
   });
 
   return (
-    <section className="hover:cursor-pointer header bg-blue-700/90 w-full h-12 xl:h-16">
+    <section
+      className={`${isOpen ? "h-lvh bg-black absolute" : "bg-blue-700/90"} hover:cursor-pointer header sm:bg-blue-700/90 w-full sm:h-12 xl:h-16`}
+    >
       <nav>
-        <ul className="mx-auto list-none w-2/3 xl:w-2/3 xl:h-16  flex gap-5 justify-between h-12 items-center font-serif">
+        <div className="menuBurger sm:hidden h-12 px-1 items-center flex ">
+          <CiMenuBurger
+            onClick={() => setIsOpen(!isOpen)}
+            className="text-white text-4xl "
+          />
+        </div>
+        <ul
+          className={` ${isOpen ? "visible h-[80vh]" : "hidden"} flex gap-10 flex-col justify-center w-full mx-auto list-none sm:w-2/3 xl:w-2/3 xl:h-16 sm:flex sm:flex-row sm:gap-5 sm:justify-between sm:h-12 items-center font-serif`}
+        >
           {navigationElements}
         </ul>
       </nav>
     </section>
   );
 }
+
+
