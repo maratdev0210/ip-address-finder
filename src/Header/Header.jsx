6e6a73fac50309e964621545c229f4b318d35b18
maratdev0210 <maratforwork1@gmail.com>
2025-02-06 01:19:12 +0300

    add dark-mode for the Header

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -2,8 +2,15 @@ import React from "react";
 import { CiMenuBurger } from "react-icons/ci";
 import { useState } from "react";
 
+const colorModes = {
+  0: "dark",
+  1: "light",
+};
+
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
+  const [toggleMode, setToggleMode] = useState(0); // 0 is for dark mode, 1 is for white mode
+
   const links = {
     "#": "Home",
     "#findIp": "Find your IP",
@@ -19,7 +26,7 @@ export default function Header() {
     return (
       <li>
         <a
-          className="text-white text-2xl xl:text-3xl hover:text-white/50 hover:underline"
+          className="text-black dark:text-white text-2xl xl:text-3xl hover:text-white/50 hover:underline"
           href={link}
           onClick={() => setIsOpen(!isOpen)}
         >
@@ -29,25 +36,35 @@ export default function Header() {
     );
   });
 
+  // TO-DO: find a better way for writing styles using Tailwind
   return (
     <section
-      className={`${isOpen ? "h-lvh bg-black absolute" : "bg-blue-700/90"} hover:cursor-pointer header sm:bg-blue-700/90 w-full sm:h-12 xl:h-16`}
+      className={` ${colorModes[toggleMode] == "light" ? "dark" : "light"} ${isOpen ? "h-lvh bg-white absolute" : "bg-blue-700/90"} dark:bg-black hover:cursor-pointer header sm:bg-blue-700/90 w-full sm:h-12 xl:h-16`}
     >
-      <nav>
-        <div className="menuBurger sm:hidden h-12 px-1 items-center flex ">
+      <nav className={`justify-between ${isOpen ? "block" : "flex"}`}>
+        <div className="menuBurger sm:hidden h-12 px-1 items-center flex">
           <CiMenuBurger
             onClick={() => setIsOpen(!isOpen)}
-            className="text-white text-4xl "
+            className={`${isOpen ? "text-black" : "text-white"} dark:text-white text-4xl`}
           />
         </div>
         <ul
-          className={` ${isOpen ? "visible h-[80vh]" : "hidden"} flex gap-10 flex-col justify-center w-full mx-auto list-none sm:w-2/3 xl:w-2/3 xl:h-16 sm:flex sm:flex-row sm:gap-5 sm:justify-between sm:h-12 items-center font-serif`}
+          className={` ${isOpen ? "visible h-[80vh]" : "hidden"} flex gap-10 flex-col justify-center w-full mx-auto list-none sm:w-2/3
+           xl:w-2/3 xl:h-16 sm:flex sm:flex-row sm:gap-5 sm:justify-between sm:h-12 items-center font-serif`}
         >
           {navigationElements}
         </ul>
+        {isOpen == false ? (
+          <div className="color-mode flex items-center px-3">
+            <span
+              className="text-white text-md font-serif sm:text-2xl xl:text-3xl"
+              onClick={() => setToggleMode(1 - toggleMode)}
+            >
+              Enable {colorModes[toggleMode]} Mode
+            </span>
+          </div>
+        ) : null}
       </nav>
     </section>
   );
 }
-
-
