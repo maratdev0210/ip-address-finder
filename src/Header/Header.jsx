2eab486d641522e05c17cb27d73f0c554d963fd1
maratdev0210 <maratforwork1@gmail.com>
2025-02-07 19:15:32 +0300

    store the color mode in localStorage

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import { useState } from "react";
+import { useRef } from "react";
+import { useEffect } from "react";
 
 import { colorModes } from "./utilities/colorModes";
 import { navigationLinks } from "./utilities/navigationLinks";
 
 import Navigation from "./Navigation";
-import DarkMode from "./DarkMode";
+import SwitchColorMode from "./SwitchColorMode";
 import BurgerMenu from "./BurgerMenu";
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
   const [toggleMode, setToggleMode] = useState(0); // 0 is for dark mode, 1 is for white mode
+  const mode = useRef(localStorage.getItem("themeMode")); // [dark | light]
+
+  useEffect(() => {
+    mode.current = localStorage.getItem("themeMode");
+  });
 
   // doesn't allow style-imposed bugs when the screen changes
   window.addEventListener("resize", (event) =>
@@ -20,7 +27,7 @@ export default function Header() {
   // TO-DO: find a better way for writing styles using Tailwind
   return (
     <section
-      className={`${colorModes[toggleMode] == "light" ? "dark" : "light"} ${isOpen ? "h-[100vh] top-0 left-0 bottom-0 right-0 w-full z-100 bg-white fixed" : "bg-blue-700/90"} dark:bg-black hover:cursor-pointer header sm:bg-blue-700/90 w-full sm:h-12 xl:h-16`}
+      className={`${mode.current} ${isOpen ? "h-[100vh] top-0 left-0 bottom-0 right-0 w-full z-100 bg-white fixed" : "bg-blue-700/90"} dark:bg-black hover:cursor-pointer header sm:bg-blue-700/90 w-full sm:h-12 xl:h-16`}
     >
       <nav className={`justify-between ${isOpen ? "block" : "flex"}`}>
         <BurgerMenu isOpen={isOpen} setIsOpen={setIsOpen} />
@@ -31,7 +38,7 @@ export default function Header() {
           setIsOpen={setIsOpen}
         />
 
-        <DarkMode
+        <SwitchColorMode
           colorModes={colorModes}
           isOpen={isOpen}
           toggleMode={toggleMode}
--- src/Header/SwitchColorMode.jsx
+++ src/Header/SwitchColorMode.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { useEffect } from "react";
 
-export default function DarkMode({
+// a text being put on top of page's header that acts as a switch between dark/light theme of a webpage
+export default function SwitchColorMode({
   colorModes,
   isOpen,
   toggleMode,
   setToggleMode,
 }) {
+  useEffect(() => {
+    localStorage.setItem("themeMode", toggleMode == 0 ? "dark" : "light");
+  });
+
   return (
     <React.Fragment>
       {!isOpen ? (
